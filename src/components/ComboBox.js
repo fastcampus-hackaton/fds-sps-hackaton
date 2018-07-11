4aab078daaeb4e2c65598243069dffe917cf55ec
chiabi <chiabi88@gmail.com>
2018-07-11 16:13:55 +0900

    Add Combo box component

---
+++ src/components/ComboBox.js
@@ -0,0 +1,76 @@
+import React, { Component } from "react";
+
+export default class ComboBox extends Component {
+  static defaultProps = {
+    options: [], // 옵션
+    selected: "", // 선택된 값
+    defaultText: "", // 디폴트 텍스트
+    onSelectItem: item => {} // 선택된 아이템
+  };
+  state = {
+    visible: false,
+    value: null
+  };
+
+  handleShow = () => {
+    this.setState({
+      visible: true
+    });
+    this.comboRef.current.focus();
+  };
+  handleClose = () => {
+    this.setState({
+      visible: false
+    });
+  };
+  handleSelect = menu => {
+    this.setState({
+      value: menu,
+      visible: false
+    });
+    this.props.onSelectItem(menu);
+  };
+  comboRef = React.createRef();
+
+  render() {
+    const { options, defaultText, selected } = this.props;
+    const { visible } = this.state;
+    return (
+      <div
+        className="combobox"
+        onBlur={this.handleClose}
+        onFocus={this.handleShow}
+        ref={this.comboRef}
+        tabIndex={visible ? 1 : 0}
+      >
+        <div
+          className={
+            selected
+              ? "combobox-select"
+              : "combobox-select combobox-select--default"
+          }
+          onClick={this.handleShow}
+        >
+          {selected
+            ? selected
+            : defaultText
+              ? defaultText
+              : "옵션을 선택해주세요"}
+        </div>
+        {visible && (
+          <div className="combobox-menu">
+            {options.map(menu => (
+              <div
+                className={selected === menu ? "selected" : ""}
+                key={menu}
+                onClick={e => this.handleSelect(menu)}
+              >
+                {menu}
+              </div>
+            ))}
+          </div>
+        )}
+      </div>
+    );
+  }
+}
