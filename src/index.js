3bfb04b37a350a9eae3ccd13fba90a9b1d2a5505
chiabi <chiabi88@gmail.com>
2018-07-11 16:13:24 +0900

    Import slick slider css

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import "../node_modules/slick-carousel/slick/slick.css";
 import "./index.css";
 import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
