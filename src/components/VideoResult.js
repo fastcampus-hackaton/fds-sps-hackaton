ca8f84870f70e1e183f1224a31552642be31a1af
Kim Sanghyeok <skyee23@gmail.com>
2018-07-12 16:02:43 +0900

    Fix confilct

--- src/components/VideoResult.js
+++ src/components/VideoResult.js
@@ -1,14 +1,25 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import withData from "../hocs/withData";
 
-export default class VideoResult extends React.Component {
+class VideoResult extends React.Component {
+  static defaultProps = {
+    kindsEdu: []
+  };
+
   componentDidMount() {
     window.scrollTo(0, 0);
   }
+
   render() {
+    const { kindsEdu, contentsId } = this.props;
+    const videoContents = kindsEdu.find(
+      item => item.id === parseInt(contentsId, 10)
+    );
     return (
       <div className="video-wrapper">
         <div className="box is-shady">
-          <h1 className="title is-3">사회화 훈련 교육자료</h1>
+          <h1 className="title is-3">{videoContents.title}</h1>
           <div className="edu-video">
             <iframe
               id="ytplayer"
@@ -16,16 +27,20 @@ export default class VideoResult extends React.Component {
               type="text/html"
               width="640"
               height="360"
-              src="https://www.youtube.com/embed/BngCfZoanbA"
-              frameborder="0"
+              src={`https://www.youtube.com/embed/${videoContents.code}`}
+              frameBorder="0"
             />
           </div>
           <div>
-            <p>반려동물의 사회화 훈련 교육이 궁금하셨죠?</p>
-            <p>지금 제대로 된 사회화 훈련 교육을 확인해보세요.</p>
+            <p>{videoContents.body}</p>
           </div>
+          <Link to="/oresult" className="button is-primary is-outlined">
+            이전 가이드라인 보러가기
+          </Link>
         </div>
       </div>
     );
   }
 }
+
+export default withData(VideoResult);
